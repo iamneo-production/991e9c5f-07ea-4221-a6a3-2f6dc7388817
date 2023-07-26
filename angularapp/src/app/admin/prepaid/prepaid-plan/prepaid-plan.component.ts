88ef0b98d2d02621cd847d8a7aca388061642d26
Bhaskar-339 <bhaskarmylavarapu339@gmail.com>
2023-07-26 11:13:03 +0000

    rmvd cmnt

--- angularapp/src/app/admin/prepaid/prepaid-plan/prepaid-plan.component.ts
+++ angularapp/src/app/admin/prepaid/prepaid-plan/prepaid-plan.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PlanModel } from 'src/app/models/plan-model';
 import { PlanService } from 'src/app/services/plan.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-prepaid-plan',
