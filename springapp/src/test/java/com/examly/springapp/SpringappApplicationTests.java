2cbc67adb41236577ffdaa96f5d1caf1c6f8d396
shriram8601 <shriramsr01@gmail.com>
2023-07-26 12:06:59 +0000

    accepted

--- springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
+++ springapp/src/test/java/com/examly/springapp/SpringappApplicationTests.java
@@ -4,7 +4,6 @@ package com.examly.springapp;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//import org.junit.Test;
 import org.junit.jupiter.api.Test; 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
