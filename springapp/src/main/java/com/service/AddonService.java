ab541be89a1ce9e06b4d929e0cb3f80935556a1e
shriram8601 <shriramsr01@gmail.com>
2023-07-27 06:22:46 +0000

    addon

---
+++ springapp/src/main/java/com/service/AddonService.java
@@ -0,0 +1,32 @@
+package main.java.com.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.model.Addon;
+import com.examly.springapp.repository.AddonRepository;
+
+@Service
+public class AddonService {
+	
+	@Autowired
+	private AddonRepository addonRepo;
+	
+	public Addon addAddon(Addon addon) {
+		return addonRepo.save(addon);
+	}
+	public List<Addon> getAllAddon(){
+		return addonRepo.findAll();
+	}
+	public Addon getAddonById(int id) {
+		return addonRepo.findById(id).get();
+	}
+	public void updateAddon(Addon addon) {
+		addonRepo.save(addon);
+	}
+	public void deleteAddon(int id) {
+		addonRepo.deleteById(id);
+	}
+}
