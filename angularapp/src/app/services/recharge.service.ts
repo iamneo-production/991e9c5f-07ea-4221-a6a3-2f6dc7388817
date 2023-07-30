f45e1799611200c7c6864cebae4366d89084e017
Shankar1307 <shankarmuthamil@gmail.com>
2023-07-30 16:30:31 +0000

    modified commented code

--- angularapp/src/app/services/recharge.service.ts
+++ angularapp/src/app/services/recharge.service.ts
@@ -1,30 +1,4 @@
-// import { HttpClient } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { RechargeModel } from '../models/recharge-model';
-// import { Observable } from 'rxjs';
-// import { LoginService } from './login.service';
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class RechargeService {
-//     // private  baseUrl = "http://localhost:3000/recharge";
-//     private  baseUrl = "https://8080-babdbceaaedaaecdababecfbdcbbefdaddee.project.examly.io/admin/recharge";
 
-//   constructor(private httpClient: HttpClient,
-//     private loginService: LoginService) { }
-
-//   name: string = '';
-//   tab: string ;
-//   userId: number = this.loginService.getUserId();
-
-//   addRecharge(recharge: RechargeModel): Observable<Object> {
-//       return this.httpClient.post(`${this.baseUrl}/${this.userId}`, recharge);
-//   }
-
-//   getAllRecharges(): Observable<RechargeModel[]>{
-//     return this.httpClient.get<RechargeModel[]>(`${this.baseUrl}/${this.userId}`);
-//   }
-// }
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
