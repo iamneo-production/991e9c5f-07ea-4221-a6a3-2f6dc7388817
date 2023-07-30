3d48e7c64fce87a3b5d8ed1a14c48f03045611f9
Maheswari2509 <basanamaheshwari01@gmail.com>
2023-07-30 18:03:31 +0000

    pull2

--- springapp/src/main/java/com/examly/springapp/controller/RechargeController.java
+++ springapp/src/main/java/com/examly/springapp/controller/RechargeController.java
@@ -13,26 +13,31 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.examly.springapp.model.Recharge;
+import com.examly.springapp.model.Message;
 import com.examly.springapp.service.RechargeService;
 
 @RestController
-@RequestMapping("/admin")
+@CrossOrigin(origins="*")
+@RequestMapping("admin")
 public class RechargeController {
 
 	@Autowired
 	private RechargeService rechargeService;
 	
-	@PostMapping("/recharge")
-	public ResponseEntity<?> addRecharge(@RequestBody Recharge recharge){
-		return new ResponseEntity<Recharge>(rechargeService.addRecharge(recharge), HttpStatus.CREATED);
+	@PostMapping("/recharge/{id}")
+	public ResponseEntity<?> addRecharge(@PathVariable Long id, @RequestBody Recharge recharge){
+		// System.out.println(id+"recharge id");
+		rechargeService.addRecharge(id,recharge);
+		return new ResponseEntity<>(new Message("recharge added successfully"), HttpStatus.CREATED);
 	}
 	
-	@GetMapping("/recharge")
-	public ResponseEntity<List<Recharge>> viewRecharge(){
-//		System.out.println(rechargeService.getRecharge());
-		return new ResponseEntity<>(rechargeService.getRecharge(), HttpStatus.OK);
+    @GetMapping("/recharge/{id}")
+	public ResponseEntity<List<Recharge>> viewRecharge(@PathVariable Long id){
+		List<Recharge> rechargeList = rechargeService.getRecharge(id);
+		return new ResponseEntity<>(rechargeList, HttpStatus.OK);
 	}
 	
 	
--- springapp/src/main/java/com/examly/springapp/repository/ReviewRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/ReviewRepo.java
@@ -1,10 +1,11 @@
 package com.examly.springapp.repository;
-import com.examly.springapp.model.Plan;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.examly.springapp.model.Review;
+
 @Repository
-public interface PlanRepo extends JpaRepository<Plan, Long>{
-    
+public interface ReviewRepo extends JpaRepository<Review,Integer>{
+
 }
